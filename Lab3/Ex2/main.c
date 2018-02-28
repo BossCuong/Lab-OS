5b55cdc60fc72a24308261f795128cceb5a80271
Boss Cuong <1610342@hcmut.edu.vn>
2018-03-01 01:03:27 +0700

    Fix Lab3-Ex2

--- Lab3/Ex2/main.c
+++ Lab3/Ex2/main.c
@@ -10,59 +10,60 @@ int main(int argc, int *argv[])
     //Process A
     printf("Process A \t%d \t\t%d \n", getpid(), getppid());
 
-    //Creat process B
-    pid_t pid = fork();
+    pid_t pid = fork(); //Creat process B
 
-    if (pid > 0) //In process A
+    if (pid > 0) //Process A
     {
         wait(NULL);
-        //Creat process C
-        pid_t pid = fork();
 
-        if (pid > 0) //In process A
+        pid_t pid = fork(); //Creat process C
+
+        if (pid > 0) //Process A
         {
             wait(NULL);
-            //Creat process D
-            pid_t pid = fork();
+
+            pid_t pid = fork(); //Creat process D
+
             if (pid == 0)
                 printf("Process D \t%d \t\t%d \n", getpid(), getppid());
             else if (pid == -1)
                 printf("error\n");
         }
-        if (pid == 0) //In process C
+        else if (pid == 0) //In process C
         {
             printf("Process C \t%d \t\t%d \n", getpid(), getppid());
 
-            //Creat process G
-            pid_t pid = fork();
+            pid_t pid = fork(); //Creat process G
+
             if (pid == 0)
                 printf("Process G \t%d \t\t%d \n", getpid(), getppid());
             else if (pid == -1)
                 printf("error\n");
         }
     }
-    else if (pid == 0) //In process B
+    else if (pid == 0) //Process B
     {
         printf("Process B \t%d \t\t%d \n", getpid(), getppid());
 
-        //Creat process E
-        pid_t pid = fork();
-        if (pid > 0) //In process B
+        pid_t pid = fork(); //Creat process E
+
+        if (pid > 0) //Process B
         {
             wait(NULL);
-            //Creat process F
-            pid_t pid = fork();
+
+            pid_t pid = fork(); //Creat process F
+
             if (pid == 0)
                 printf("Process F \t%d \t\t%d \n", getpid(), getppid());
             else if (pid == -1)
                 printf("error\n");
         }
-        else if (pid == 0) //In process E
+        else if (pid == 0) //Process E
         {
             printf("Process E \t%d \t\t%d \n", getpid(), getppid());
 
-            //Creat process I
-            pid_t pid = fork();
+            pid_t pid = fork(); //Creat process I
+
             if (pid == 0)
                 printf("Process I \t%d \t\t%d \n", getpid(), getppid());
             else if (pid == -1)
